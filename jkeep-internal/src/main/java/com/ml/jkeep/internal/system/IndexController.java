7489a1ffc14aa2baefb4251b5fd567041739e01a
谭良忠 <tanliangzhong@beadwallet.com>
2019-07-26 17:36:41 +0800

    主页修改.

--- jkeep-internal/src/main/java/com/ml/jkeep/internal/system/IndexController.java
+++ jkeep-internal/src/main/java/com/ml/jkeep/internal/system/IndexController.java
@@ -1,14 +1,10 @@
 package com.ml.jkeep.internal.system;
 
 import com.ml.jkeep.common.controller.BaseController;
-import com.ml.jkeep.internal.auth.JKeepSecurityContextHolder;
-import com.ml.jkeep.service.system.impl.HrefPermissionService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 主页 - Controller
@@ -21,24 +17,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping
 public class IndexController extends BaseController {
 
-    @Autowired
-    private HrefPermissionService hrefPermissionService;
-
     @GetMapping("/")
     public String index() {
-        log.info("User:{}", JKeepSecurityContextHolder.getUserInfo().getUsername());
-        hrefPermissionService.hrefPermission(1L);
-        hrefPermissionService.hrefPermission();
+        // TODO 获取用户菜单.
         return "views/index";
     }
-
-    @ResponseBody
-    @GetMapping("/hello")
-    public String hello(String name) {
-        hrefPermissionService.hrefPermission(1L);
-        hrefPermissionService.hrefPermission();
-        return "Hello " + name;
-    }
-
 
 }
