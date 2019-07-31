c1b349b61badcf20437e70ccf6b9599efaf88927
谭良忠 <tanliangzhong@beadwallet.com>
2019-07-31 17:21:13 +0800

    优化代码.

--- jkeep-common/src/main/java/com/ml/jkeep/common/bo/PageBo.java
+++ jkeep-common/src/main/java/com/ml/jkeep/common/bo/PageBo.java
@@ -2,9 +2,6 @@ package com.ml.jkeep.common.bo;
 
 import lombok.Data;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 /**
@@ -19,16 +16,11 @@ public class PageBo<T> implements Serializable {
     /**
      * 页码
      */
-    @NotNull(message = "起始页为空")
-    @Min(value = 1, message = "起始页最小1")
     private Integer page;
 
     /**
      * 页码大小
      */
-    @NotNull(message = "页大小为空")
-    @Min(value = 5, message = "页大小最小5")
-    @Max(value = 100, message = "页大小最大100")
     private Integer size;
 
     /**
