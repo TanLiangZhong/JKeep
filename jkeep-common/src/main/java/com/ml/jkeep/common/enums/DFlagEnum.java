5176ae2509338d2a72eebb2329dc73c05d5e2192
谭良忠 <tanliangzhong@beadwallet.com>
2019-09-09 16:20:57 +0800

    添加注释

--- jkeep-common/src/main/java/com/ml/jkeep/common/enums/DFlagEnum.java
+++ jkeep-common/src/main/java/com/ml/jkeep/common/enums/DFlagEnum.java
@@ -14,10 +14,10 @@ import lombok.Getter;
 public enum DFlagEnum {
 
     /**
-     * 删除标记
+     * 删除标记 (1-正常 0-删除)
      */
-    NORMAL((byte) 0, "未删除"),
-    DELETE((byte) 1, "已删除");
+    NORMAL((byte) 1, "正常"),
+    DELETE((byte) 0, "删除");
 
     private Byte code;
     private String name;
